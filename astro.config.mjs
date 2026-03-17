6349d588168ab21f14e1ab03197f73aa68a12feb
Kiko R <1696627@alu.murciaeduca.es>
2026-03-17 17:55:57 +0100

    new server route

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     output: "static",
-    // base: "/UNIKO/",
+    base: "/gra126/kiko_rg/uniko/",
     vite: {
         resolve: {
             alias: {
